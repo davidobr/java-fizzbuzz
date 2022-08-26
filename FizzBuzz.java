6ce3be6360e00e55468ee7cd7a83a62afb4cebd3
davidobr <david.obrien.nord@gmail.com>
2022-08-26 07:57:14 -0700

    Created fizzbuzz functionality

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,5 +1,16 @@
 public class FizzBuzz {
     public static void main(String[] args){
-        
+        for(int x = 1; x <= 15; x++){
+            if(x % 3 == 0 && x % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else if(x % 3 == 0){
+                System.out.println("Fizz");
+            }else if(x % 5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(x);
+            }
+            
+        }
     }
 }

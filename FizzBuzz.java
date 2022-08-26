2bec623bd2b9ce74839717ec571c5493c3af0df0
davidobr <david.obrien.nord@gmail.com>
2022-08-26 07:59:05 -0700

    Extended loop to print out 100 times

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public static void main(String[] args){
-        for(int x = 1; x <= 15; x++){
+        for(int x = 1; x <= 100; x++){
             if(x % 3 == 0 && x % 5 == 0){
                 System.out.println("FizzBuzz");
             }else if(x % 3 == 0){
@@ -10,7 +10,6 @@ public class FizzBuzz {
             }else{
                 System.out.println(x);
             }
-            
         }
     }
 }
